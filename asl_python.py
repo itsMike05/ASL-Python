0a5d3fcaf469449608a7b893108a59fdf5769a6b
Michał Pelc <michal.pelc@live.com>
2023-02-06 19:24:50 +0100

    Implemented the camera output and some basic hand tracking

--- asl_python.py
+++ asl_python.py
@@ -1,3 +1,13 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
 
+# Initializing the capture object and the hand detector
+cap = cv2.VideoCapture(0)
+detector = HandDetector(maxHands=1)
+
+# Displaying the camera output
+while True:
+    success, img = cap.read()
+    hands, img = detector.findHands(img)
+    cv2.imshow("ASL", img)
+    cv2.waitKey(1)
