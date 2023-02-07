65b6e7ed09a764a10dfae256990ab61edc4aca36
Michał Pelc <michal.pelc@live.com>
2023-02-07 10:45:43 +0100

    Centering the image

--- data_collection.py
+++ data_collection.py
@@ -1,6 +1,7 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
 import numpy as np
+import math
 
 # Initializing the capture object and the hand detector
 cap = cv2.VideoCapture(0)
@@ -16,11 +17,27 @@ while True:
     if hands:
         hand = hands[0]
         x, y, w, h = hand["bbox"]  # Bounding box for the hand (x, y, width, height)
-        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255 # Creating a blank white image
+        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255  # Creating a blank white image
         imgCrop = img[y - crop_offset: y + h + crop_offset, x - crop_offset: x + w + crop_offset]
-        imgCropSize = imgCrop.shape
 
-        imgWhite[0 : imgCropSize[0], 0 : imgCropSize[1]] = imgCrop
+        aspectRatio = h / w
+        if aspectRatio > 1:
+            k = imgSize / h  # Aspect ratio constant
+            widthCal = math.ceil(k * w)
+            imgResize = cv2.resize(imgCrop, (widthCal, imgSize))
+            imgResizeShape = imgResize.shape
+            # Centering the image
+            widthGap = math.ceil((imgSize - widthCal) / 2)
+            imgWhite[:, widthGap: widthCal + widthGap] = imgResize
+        else:
+            k = imgSize / w  # Aspect ratio constant
+            heightCal = math.ceil(k * h)
+            imgResize = cv2.resize(imgCrop, (imgSize, heightCal))
+            imgResizeShape = imgResize.shape
+            # Centering the image
+            heightGap = math.ceil((imgSize - heightCal) / 2)
+            imgWhite[heightGap: heightCal + heightGap, :] = imgResize
+
 
         cv2.imshow("Img Crop", imgCrop)
         cv2.imshow("Img White", imgWhite)
